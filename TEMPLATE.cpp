abe725a782720b384ed0ff04089015497f9453f7
Miguel Araujo <miguelarauj1o@gmail.com>
2015-01-19 21:32:45 -0300

    template file included

---
+++ TEMPLATE.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+
+#define EPS 1e-5
+#define PI (acos(-1))
+#define INF 999999999
+#define ST first
+#define ND second
+#define MP make_pair
+#define PB push_back
+
+#define _ printf("\n");
+#define SC1(n) scanf("%d", &n);
+#define SC2(a, b) scanf("%d %d", &a);
+#define SC3(a, b, c) scanf("%d %d %d", &a, &b, &c);
+#define SIZE(a) ((int)((a).size()))
+#define VAR(a,b) __typeof(b) a=(b)
+#define ZERO(x) memset(x, 0, sizeof(x))
+#define CLEAR(x, a) memset(x, a, sizeof(x))
+#define FOR(i, n) for(int i = 0; i < (n); ++i)
+#define REP(i, a, b) for (int i = (a); i < (b); ++i)
+#define isLetter(ch) (((ch) >= 'A' && (ch) <= 'Z') || ((ch) >= 'a' && (ch) <= 'z'))
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef pair<int,int> PII;
+typedef set<int> SI;
+typedef long long int LLI;
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ULL;
+
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
