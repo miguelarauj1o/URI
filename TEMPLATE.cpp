6b6589366a87ccc3ddbbbd99fd38850c5f828cd8
Miguel Araújo <miguelarauj1o@gmail.com>
2015-03-02 11:53:55 -0300

    TEMPLATE updated

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,67 +1,44 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#include <vector>
+#include <algorithm>
 #include <cmath>
 #include <climits>
 #include <complex>
 #include <iostream>
-#include <algorithm>
 #include <iomanip>
 #include <string>
 #include <sstream>
 #include <utility>
-#include <vector>
 #include <queue>
 #include <stack>
 #include <list>
 #include <map>
 #include <set>
 
-#define _CRT_SECURE_NO_DEPRECATE //suppress some compilation warning messages
-
-#define F first
-#define S second
-#define MP make_pair
-#define PB push_back
 #define EPS 1e-9
 #define PI 3.141592654
-#define INF 0x3f3f3f3f
-#define LINF 0x3f3f3f3f3f3f3f3fLL
+#define INF 1000000000
+#define mp make_pair
+#define pb push_back
+#define sc1(a) scanf("%d", &a)
+#define sc2(a, b) scanf("%d %d", &a, &b)
+#define sc3(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define sc4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define size(a) ((int)((a).size()))
+#define var(a,b) __typeof(b) a=(b)
+#define zero(x) memset(x, 0, sizeof(x))
+#define clear(x, a) memset(x, a, sizeof(x))
+#define for(i, n) for(int i = 0; i < (n); ++i)
+#define isLetter(c) (((c) >= 'A' && (c) <= 'Z') || ((c) >= 'a' && (c) <= 'z'))
 
-#define SC1(a) scanf("%d", &a)
-#define SC2(a, b) scanf("%d %d", &a, &b)
-#define SC3(a, b, c) scanf("%d %d %d", &a, &b, &c)
-#define SC4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
-#define SC5(a, b, c, d, e) scanf("%d %d %d %d %d", &a, &b, &c, &d, &e)
-#define SIZE(a) ((int)((a).size()))
-#define VAR(a,b) __typeof(b) a=(b)
-#define ZERO(x) memset(x, 0, sizeof(x))
-#define CLEAR(x, a) memset(x, a, sizeof(x))
-#define FOR(i, n) for(int i = 0; i < (n); ++i)
-#define REP(i, a, b) for (int i = (a); i < (b); ++i)
-#define isLETTER(c) (((c) >= 'A' && (c) <= 'Z') || ((c) >= 'a' && (c) <= 'z'))
-
-typedef long long LL;
-typedef long long int LLI;
-typedef long double LD;
-typedef unsigned long long ULL;
-
-typedef set<int> EI;
-typedef list<int> LI;
-typedef stack<int> SI;
-typedef vector<int> VI;
-typedef map<char, int> MCI;
-typedef pair<int, int> PII;
-
-#ifndef ONLINE_JUDGE 
-   // URI flag, code inside here will be not processed
-#endif
-
-template <class T> string toStr(const T &x)
-{ stringstream s; s << x; return s.str(); }
-
-template <class T> int toInt(const T &x)
-{ stringstream s; s << x; int r; s >> r; return r; }
+typedef long long 		ll;
+typedef list<int> 		li;
+typedef stack<int> 		si;
+typedef vector<int> 	vi;
+typedef pair<int, int> 	ii;
+typedef vector<ii>		vii;
 
 using namespace std;
 
