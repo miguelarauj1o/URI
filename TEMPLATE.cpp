d3662d02e704302b46fea6fd74872ff8cd4cbb11
Miguel Araújo <miguelarauj1o@gmail.com>
2015-03-11 12:26:53 -0300

    TEMPLATE updated, again

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,3 +1,7 @@
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 #include <vector>
 #include <list>
 #include <map>
@@ -12,42 +16,35 @@
 #include <sstream>
 #include <iostream>
 #include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <math.h>
 #include <limits.h>
  
 using namespace std;
- 
-// Datatypes
-typedef vector<int> vi; 
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define sz(a) int((a).size())
+#define all(c) (c).begin(),(c).end()
+#define clear(x, a) memset(x, a, sizeof(x))
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define for(i,a,n) for(int (i) = (a); (i) < (n); (i)++)
+#define sc1(a) scanf("%d", &a)
+#define sc2(a,b) scanf("%d %d", &a, &b)
+#define sc3(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define sc4(a,b,c,d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define isLetter(c) (((c) >= 'A' && (c) <= 'Z') || ((c) >= 'a' && (c) <= 'z'))
+
+typedef vector<int> vi;
 typedef vector<string> vs;
-typedef vector<vi> vvi; 
-typedef pair<int,int> ii; 
+typedef vector<vi> vvi;
+typedef stack<int> si;
+typedef pair<int,int> ii;
 typedef long long ll;
 typedef long double ld;
  
-// Define Macros
-#define fi 					first
-#define se 					second
-#define mp 					make_pair
-#define pb                	push_back
-#define sz(a)             	int((a).size())  
-#define all(c)            	(c).begin(),(c).end()
-#define clear(x, a) 		memset(x, a, sizeof(x))
-#define present(c,x)      	((c).find(x) != (c).end())
-#define cpresent(c,x)     	(find(all(c),x) != (c).end()) 
-#define for(i,n)        	for(int (i) = 0; (i) < (n); (i)++)
-#define sc1(a) 				scanf("%d", &a)
-#define sc2(a,b) 			scanf("%d %d", &a, &b)
-#define sc3(a,b,c) 			scanf("%d %d %d", &a, &b, &c)
-#define sc4(a,b,c,d) 		scanf("%d %d %d %d", &a, &b, &c, &d)
-#define isLetter(c) 		(((c) >= 'A' && (c) <= 'Z') || ((c) >= 'a' && (c) <= 'z'))
- 
-// Constants
-const double eps = 1e-8;
+const double EPS = 1e-8;
 const double PI = 3.1415926535897932384626433832795;
 
 int main(void)
