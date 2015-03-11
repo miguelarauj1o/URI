c34bf27b7f233da40d753ec4a0bde327e537d757
Miguel Araújo <miguelarauj1o@gmail.com>
2015-03-11 09:44:37 -0300

    TEMPLATE updated

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,48 +1,56 @@
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
 #include <vector>
-#include <algorithm>
-#include <cmath>
-#include <climits>
-#include <complex>
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <sstream>
-#include <utility>
-#include <queue>
-#include <stack>
 #include <list>
 #include <map>
 #include <set>
-
-#define EPS 1e-9
-#define PI 3.141592654
-#define INF 1000000000
-#define mp make_pair
-#define pb push_back
-#define sc1(a) scanf("%d", &a)
-#define sc2(a, b) scanf("%d %d", &a, &b)
-#define sc3(a, b, c) scanf("%d %d %d", &a, &b, &c)
-#define sc4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
-#define size(a) ((int)((a).size()))
-#define var(a,b) __typeof(b) a=(b)
-#define zero(x) memset(x, 0, sizeof(x))
-#define clear(x, a) memset(x, a, sizeof(x))
-#define for(i, n) for(int i = 0; i < (n); ++i)
-#define isLetter(c) (((c) >= 'A' && (c) <= 'Z') || ((c) >= 'a' && (c) <= 'z'))
-
-typedef long long 		ll;
-typedef list<int> 		li;
-typedef stack<int> 		si;
-typedef vector<int> 	vi;
-typedef pair<int, int>	ii;
-typedef vector<ii>		vii;
-
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
+#include <limits.h>
+ 
 using namespace std;
+ 
+// Datatypes
+typedef vector<int> vi; 
+typedef vector<string> vs;
+typedef vector<vi> vvi; 
+typedef pair<int,int> ii; 
+typedef long long ll;
+typedef long double ld;
+ 
+// Define Macros
+#define fi 					first
+#define se 					second
+#define mp 					make_pair
+#define pb                	push_back
+#define sz(a)             	int((a).size())  
+#define all(c)            	(c).begin(),(c).end()
+#define clear(x, a) 		memset(x, a, sizeof(x))
+#define present(c,x)      	((c).find(x) != (c).end())
+#define cpresent(c,x)     	(find(all(c),x) != (c).end()) 
+#define for(i,n)        	for(int (i) = 0; (i) < (n); (i)++)
+#define sc1(a) 				scanf("%d", &a)
+#define sc2(a,b) 			scanf("%d %d", &a, &b)
+#define sc3(a,b,c) 			scanf("%d %d %d", &a, &b, &c)
+#define sc4(a,b,c,d) 		scanf("%d %d %d %d", &a, &b, &c, &d)
+#define isLetter(c) 		(((c) >= 'A' && (c) <= 'Z') || ((c) >= 'a' && (c) <= 'z'))
+ 
+// Constants
+const double eps = 1e-8;
+const double PI = 3.1415926535897932384626433832795;
 
-int main(int argc, char const *argv[])
+int main(void)
 {
 	return 0;
 }
