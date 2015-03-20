d401bf5bfbd5e81e9038954dbd9abc5f7f4fa127
Miguel Araújo <miguelarauj1o@gmail.com>
2015-03-20 00:51:43 -0300

    removing specific includes

--- template.cpp
+++ template.cpp
@@ -1,46 +1,37 @@
 #include <cstdio>
-#include <cmath>
+#include <cstring>
 #include <cstdlib>
-#include <ctime>
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-#include <deque>
-#include <stack>
-#include <bitset>
+#include <cmath>
 #include <algorithm>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <sstream>
+#include <string>
 #include <iostream>
 #include <iomanip>
-#include <limits.h>
- 
+#include <sstream>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
 using namespace std;
 
 #define fi first
 #define se second
 #define mp make_pair
 #define pb push_back
-#define sz(a) int((a).size())
-#define all(c) (c).begin(),(c).end()
-#define clear(x, a) memset(x, a, sizeof(x))
-#define present(c,x) ((c).find(x) != (c).end())
-#define cpresent(c,x) (find(all(c),x) != (c).end())
-#define for(i,a,n) for(int (i) = (a); (i) < (n); (i)++)
 #define sc1(a) scanf("%d", &a)
 #define sc2(a,b) scanf("%d %d", &a, &b)
 #define sc3(a,b,c) scanf("%d %d %d", &a, &b, &c)
 #define sc4(a,b,c,d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define for(i,a,n) for(int (i) = (a); (i) < (n); (i)++)
 #define isLetter(c) (((c) >= 'A' && (c) <= 'Z') || ((c) >= 'a' && (c) <= 'z'))
 
 typedef vector<int> vi;
-typedef vector<string> vs;
-typedef vector<vi> vvi;
 typedef stack<int> si;
 typedef pair<int,int> ii;
+typedef map<char,int> mi;
 typedef long long ll;
 typedef long double ld;
  
