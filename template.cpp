cc563c8b1b560c311be712be2fa04a79486b5d08
Miguel Araújo <miguelarauj1o@gmail.com>
2015-03-26 11:02:56 -0300

    template updated

--- template.cpp
+++ template.cpp
@@ -14,6 +14,7 @@
 #include <set>
 #include <stack>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
@@ -25,6 +26,7 @@ using namespace std;
 #define sc2(a,b) scanf("%d %d", &a, &b)
 #define sc3(a,b,c) scanf("%d %d %d", &a, &b, &c)
 #define sc4(a,b,c,d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define clear(x, a) memset(x, a, sizeof x)
 #define for(i,a,n) for(int (i) = (a); (i) < (n); (i)++)
 #define isLetter(c) (((c) >= 'A' && (c) <= 'Z') || ((c) >= 'a' && (c) <= 'z'))
 
@@ -38,7 +40,7 @@ typedef long double ld;
 const double EPS = 1e-8;
 const double PI = 3.1415926535897932384626433832795;
 
-int main(void)
+int main(int argc, char const *argv[])
 {
 	return 0;
 }
