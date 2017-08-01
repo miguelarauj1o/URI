0bec93ea27a529d57c6c7f679dde5c0d9a253ef1
Miguel Araújo <miguelarauj1o@gmail.com>
2017-07-31 21:58:27 -0300

    Up to 410
    It's time to say goodbye.

---
+++ src/URI_2483 - (7474762) - Accepted.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  int n;
+  cin >> n;
+  cout << "Feliz nat";
+  for (int i = 0; i < n; i++) cout << "a";
+  cout << "l!" << endl;
+  return 0;
+}
---
+++ src/URI_2581 - (7474691) - Accepted.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  int n;
+  std::cin >> n;
+  for (int i = 0; i < n; i++)
+    std::cout << "I am Toorg!" << std::endl;
+  return 0;
+}
